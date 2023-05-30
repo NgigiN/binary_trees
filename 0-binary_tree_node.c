f4f6c67b0c73d93b3b4fc9510e26b30650da8958
NgigiN <ngigi.nyongo@gmail.com>
2023-05-30 20:24:58 +0300

    solving seg fault

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,21 +1,27 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_node - creates a node
+ * @parent: is a pointer to root
+ * @value: is integer value to be for the node
+ * Return: pointer to the new node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (!parent)
-		return NULL;
+	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-	new_node = malloc(sizeof(binary_tree_t));
-
-	if (!new_node)
+	if (new_node == NULL)
 		return NULL;
 
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	new_node->parent = parent;
+
+	if (parent == NULL)
+		new_node->parent = NULL;
+	else
+		new_node->parent = parent;
 
 	return (new_node);
 }
