406adaccb6c35b16a368d4ecd8f748fa69c5223d
NgigiN <ngigi.nyongo@gmail.com>
2023-06-01 15:06:26 +0300

    adding remainder files and header file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree.
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the
+ * balance factor
+ * Return: balance factor of the tree, if tree is NULL return 0
+ */
+
+int max(int a, int b)
+{
+	return (a > b) ? a : b;
+}
+
+int height(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	return 1 + max(height(node->left), height(node->right));
+}
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height;
+	int right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = height(tree->left);
+	right_height = height(tree->right);
+
+	return abs(left_height - right_height);
+}
---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,78 @@
+#include <math.h>
+#include "binary_trees.h"
+
+/**
+ * max - returns the maximum of two
+ * @left: first number
+ * @right: second number
+ * Return: larger number
+ **/
+size_t max(size_t left, size_t right)
+{
+	if (left >= right)
+		return (left);
+	return (right);
+}
+
+/**
+ * count_nodes - counts nodes of the tree
+ * @tree: pointer to root node of tree
+ * Return: total number of nodes or 0
+ **/
+size_t count_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + count_nodes(tree->left) + count_nodes(tree->right));
+}
+
+/**
+ * count_height - calculates height, considering root
+ * at height 1.
+ * @tree: pointer to the root of the tree
+ * Return: height of tree or 0
+ **/
+size_t count_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + max(count_height(tree->left), count_height(tree->right)));
+}
+
+/**
+ * cal_power - calculates the power of 2
+ * @exponent: power of 2
+ * Return: 2^n
+ **/
+size_t cal_power(size_t exponent)
+{
+	size_t result = 1;
+
+	if (exponent == 0)
+		return (result);
+	while (exponent > 0)
+	{
+		result *= 2;
+		exponent -= 1;
+	}
+	return (result);
+}
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree:  pointer to the root node of the tree
+ * Return: 1 if tree is perfect, 0 if not or tree is NULL
+ **/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t total_nodes, height;
+
+	if (!tree)
+		return (0);
+	height = count_height(tree);
+	total_nodes = count_nodes(tree);
+
+	if (total_nodes == cal_power(height) - 1)
+		return (1);
+	return (0);
+}
