b1ed77f6ccbdcfbd785d7687cbeb404c74ef53f1
NgigiN <ngigi.nyongo@gmail.com>
2023-06-01 10:39:30 +0300

    correcting 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,8 +16,19 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
+	if ((tree->left == NULL) && (tree->right == NULL))
+	{
+		return (0);
+	}
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (1 + max(left_height, right_height));
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
+	}
 }
