c4dbc91277fed29ff96a950f35e8dba8e18ade98
NgigiN <ngigi.nyongo@gmail.com>
2023-06-01 15:37:28 +0300

    debugging 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,46 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * max  - finds the max of two numbers
- * @a: is the first number
- * @b: is the second number
- * Return: the larger number
- */
-
-int max(int a, int b)
-{
-	return ((a > b) ? a : b);
-}
-/**
- * height - finds the height of the tree
- * @node: is a pointer to the root of the tree
+ * binary_tree_height - finds the height of the tree
+ * @node: a pointer to the root of the tree
  * Return: height of the tree
  */
-
-int height(const binary_tree_t *node)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (node == NULL)
-		return (0);
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	return (1 + max(height(node->left), height(node->right)));
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
 
 /**
  * binary_tree_balance - checks if a binary tree is balanced
- * @tree: is node to the root node of the tree
+ * @tree: a pointer to the root node of the tree
  * Return: 1 if balanced, 0 if unbalanced or NULL
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height;
-	int right_height;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	if (tree == NULL)
-		return (0);
-
-	left_height = height(tree->left);
-	right_height = height(tree->right);
-
-	return (abs(left_height - right_height));
+	return (0);
 }
